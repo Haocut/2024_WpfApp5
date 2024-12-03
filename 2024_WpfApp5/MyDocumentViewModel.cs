b56e0ec13d5917be9bc885ac488a824238a76d47
Haocut <4b2g0126@office.stust.edu.tw>
2024-12-03 15:51:47 +0800

    更新 MyDocumentViewer 和 MainWindow 標題
    * [MainWindow.xaml]
      * 將視窗標題從 "2024小作家ver1" 更改為 "2024小作家ver2"。
    
    * [MyDocumentViewer.xaml]
      * 新增 `d:DataContext="{d:DesignInstance Type=local:MyDocumentViewModel}"` 到 `xmlns:local`。
      * 為 `boldButton`、`italicButton` 和 `underlineButton` 新增 `Command` 屬性，分別對應 `EditingCommands.ToggleBold`、`EditingCommands.ToggleItalic` 和 `EditingCommands.ToggleUnderline`。
      * 為 `fontColorPicker` 新增 `SelectedColorChanged` 事件處理程序。
      * 為 `fontFamilyComboBox` 和 `fontSizeComboBox` 新增 `SelectionChanged` 事件處理程序。
      * 為 `rtbEditor` 新增 `SelectionChanged` 事件處理程序。
    
    * [MyDocumentViewer.xaml.cs]
      * 新增 `using System.Windows.Documents;`。
      * 在 `MyDocumentViewer` 建構函式中，調整 `fontFamilyComboBox` 和 `fontSizeComboBox` 的初始化格式。
      * 新增 `fontColorPicker_SelectedColorChanged`、`fontFamilyComboBox_SelectionChanged`、`fontSizeComboBox_SelectionChanged` 和 `rtbEditor_SelectionChanged` 事件處理程序。
    
    * [MyDocumentViewModel.cs]
      * 新增 `MyDocumentViewModel` 類別，實作 `INotifyPropertyChanged` 介面，並包含 `SetProperty` 方法以支援屬性變更通知。

---
+++ 2024_WpfApp5/MyDocumentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace _2024_WpfApp5
+{
+    public class MyDocumentViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!(object.Equals(field, newValue)))
+            {
+                field = (newValue);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
